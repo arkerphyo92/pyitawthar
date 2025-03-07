410f9abda55bb18a4153eed262db33d72e02d7c6
arkerphyo92 <arkerphyo92@gmail.com>
2025-03-07 11:26:18 +0700

    Feat: Added Single Category API Call, Updated category reply in product list

--- osales/business/api/categories.py
+++ osales/business/api/categories.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from ninja import Router, Query
 from ninja.pagination import paginate, PageNumberPagination
 from business.models import ProductCategory
@@ -11,3 +12,9 @@ categoriesapi = Router(tags=["Categories"])
 def product_category_list(request, filters: CategoryFilterSchema = Query(...)):
     product_categories = ProductCategory.objects.all().select_related('parent')
     return filters.filter(product_categories)
+
+
+@categoriesapi.get("/{category_name}", response=CategorySchemaOut)
+def get_category(request, category_name: str):
+    category = get_object_or_404(ProductCategory, name=category_name)
+    return category
